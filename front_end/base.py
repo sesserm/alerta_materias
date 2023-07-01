1c1e6c1e13a437a3253dec9fc4ae0079c726a261
msesser <sessermatias@gmail.com>
2023-07-01 17:40:09 -0300

    Agrego renderizado de iconos en html

--- front_end/base.py
+++ front_end/base.py
@@ -61,6 +61,11 @@ output.write(
     "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n")
 output.write("<title>Genera tu alerta</title>\n")
 output.write("<link rel=\"stylesheet\" href=\"front_end/styles.css\">\n")
+output.write("<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"assets/apple-touch-icon.png\">\n")
+output.write("<link rel=\"icon\" sizes=\"32x32\" type=\"image/png\" href=\"assets/favicon-32x32.png\">\n")
+output.write("<link rel=\"icon\" sizes=\"16x16\" type=\"image/png\" href=\"assets/favicon-16x16.png\">\n")
+output.write("<link rel=\"manifest\" href=\"assets/site.webmanifest\">\n")
+
 output.write("</head>\n")
 output.write("<body>\n")
 output.write("<div class=\"container\">\n")
