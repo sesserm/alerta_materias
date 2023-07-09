3b0f3a98d96ef21c3070625d6929103bc6e9b5b5
msesser <sessermatias@gmail.com>
2023-07-09 14:30:21 -0300

    Modifico mensaje de notificacion en calendario.py

--- codigo_python/calendario.py
+++ codigo_python/calendario.py
@@ -169,8 +169,9 @@ if len(codigos) == len(materias) == len(fechas) == len(enlaces_info):
             join_query = sql.SQL("SELECT c.codigo, c.materia, c.fecha, c.nota_disponible, u.usuario, u.nota_disponible, u.medio, u.fecha_solicitud FROM alerta_facultad.calendario AS c INNER JOIN alerta_facultad.usuarios AS u ON c.codigo = u.codigo AND c.fecha = u.fecha_evaluacion")
             cursor.execute(join_query)
             join_results = cursor.fetchall()
+            notificaciones_enviadas = set()
             for row in join_results:
-                if row[3] == 'SI' and row[5] == 'NO':
+                if row[3] == 'SI' and row[5] == 'NO' and (row[0], row[1], row[2]) not in notificaciones_enviadas:
                     codigo = row[0]
                     materia = row[1]
                     fecha_postgres = row[2].strftime("%Y-%m-%d")
@@ -180,8 +181,15 @@ if len(codigos) == len(materias) == len(fechas) == len(enlaces_info):
                     fecha_solicitud = row[7]
                     DESTINATARIOS = [usuario]
                     ASUNTO = 'NOTA DISPONIBLE ALERTA-FCEA '
-                    MENSAJE = f'Hola!\n\nYa se encuentra disponible la nota para {materia} del {fecha_mails}.\n\nRecuerda estar pendiente a la grilla oficial en caso de que se den cambios en la nota!.\n\nSaludos.'
+                    MENSAJE = f"""¡Hola!
+
+                    Te informamos que ya se encuentra disponible la nota correspondiente a {row[1]}, la cual tuvo lugar el {fecha_mails}.
+
+                    Recuerda estar al pendiente de la grilla oficial en caso de que se realicen modificaciones posteriores a esta notificación.
+
+                    ¡Saludos!"""
                     enviar_correo(DESTINATARIOS, ASUNTO, MENSAJE)
+                    notificaciones_enviadas.add((row[0], row[1], row[2]))
                     # Insertar registro en tabla historica y borrar de usuarios
                     try:
                         insert_query = sql.SQL(
